1712afd135d64807a31111363d9cfa8f54b2d2ec
tiesa <tiesa@145.94.228.174>
2018-10-19 10:03:46 +0200

    Added main file and objective function

---
+++ Main.m
@@ -0,0 +1,10 @@
+%{
+This is the main executable file for the optimisation
+Use this file to edit process flow
+%}
+
+% Run WingInitialiser
+
+% Create Design Vector
+
+% fmincon
---
+++ Obj.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Obj(inputArg1,inputArg2)
+%OBJ The objective function for the optimisation
+%   This is the function calling all the analysis disciplines
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
